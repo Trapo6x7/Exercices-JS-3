2ee82d6a2c544dbe35ecf6a4fecbd187fa90a3d7
Trapo6x7 <matoufdanslezoo@gmail.com>
2024-11-19 10:36:42 +0100

    EXO2

---
+++ Exercice_2/script.js
@@ -0,0 +1,16 @@
+const afficher = document.querySelector("#afficher");
+const masquer = document.querySelector("#masquer");
+const text = document.querySelector("#text")
+
+afficher.addEventListener("click", handleAfficherTexte);
+masquer.addEventListener("click", handleMasquerTexte);
+
+function handleAfficherTexte(){
+
+    text.style.display = "block"
+}
+
+function handleMasquerTexte(){
+
+    text.style.display = "none"
+}
